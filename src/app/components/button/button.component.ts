23d91b5908b2cf6628cd3bd761c2064311ce8a42
CaioVictor3 <caiovictorarantes@gmail.com>
2025-09-09 13:52:37 -0300

    Alteracoes 1.0

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+
+  @Input() textoBotao!: string;
 
 }
